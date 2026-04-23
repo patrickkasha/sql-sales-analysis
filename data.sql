04143b6e0415950963cf0dffc9fd7f3719554b37
patrickkasha <patrickkasha4@gmail.com>
2026-04-23 17:26:11 -0400

    Add initial data for customers, products, and orders

---
+++ data.sql
@@ -0,0 +1,35 @@
+INSERT INTO customers VALUES
+(1, 'John Doe', 'USA'),
+(2, 'Mary Wanjiku', 'Kenya'),
+(3, 'Ali Khan', 'UAE'),
+(4,'kim','uk'),
+(5,'sonia','canada'),
+(6,'mary','denmark'),
+(7,'sam','scottland');
+
+INSERT INTO products VALUES
+(1, 'Laptop', 'Electronics', 800),
+(2, 'Phone', 'Electronics', 500),
+(3, 'Shoes', 'Fashion', 100),
+(4, 'bycicle','automotive',300),
+(5,'charger','electronic',400),
+(6, 'mouse','electronic',200),
+(7, 'chair', 'furniture',700);
+
+INSERT INTO orders VALUES
+(1, 2, '2025-03-01', 900),
+(2, 1, '2025-03-02', 500),
+(3, 2, '2025-03-05', 100),
+(4,4, '2025-03-06',130),
+(5,3,'2025-03-09', 320),
+(6,5,'2025-04-23',400),
+(7,3,'2025-05-12',600);
+
+INSERT INTO order_items VALUES
+(1, 1, 1, 1),
+(2, 1, 3, 1),
+(3, 2, 2, 1),
+(4, 3, 3, 1),
+(5, 4, 1, 1),
+(6, 3, 2, 5),
+(7, 4, 2, 3);
